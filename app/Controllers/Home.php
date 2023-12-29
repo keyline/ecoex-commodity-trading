0bfe5164706ec1b7778128a9301d594d2292dde1
debojyti <debojyti@keylines.net>
2023-12-29 13:44:09 +0530

    delete account request from frontend

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -28,7 +28,97 @@ class Home extends BaseController
         $data['title']              = $data['general_settings']->site_name;
         $data['page_header']        = $data['general_settings']->site_name;
         $data['page_content']       = $this->common_model->find_data('ecomm_pages', 'row', ['id' => 3]);
+
+        if($this->request->getMethod() == 'post') {
+            $postData   = array(
+                'user_type'             => $this->request->getPost('user_type'),
+                'entity_name'           => $this->request->getPost('entity_name'),
+                'email'                 => $this->request->getPost('email'),
+                'is_email_verify'       => $this->request->getPost('is_email_verify'),
+                'phone'                 => $this->request->getPost('phone'),
+                'is_phone_verify'       => $this->request->getPost('is_phone_verify'),
+                'comments'              => $this->request->getPost('comments'),
+            );
+            // pr($postData);
+            $this->common_model->save_data('ecomm_delete_account_requests', $postData, '', 'id');            
+            $this->session->setFlashdata('success_message', 'Delete Account Request Submitted Successfully. We Will Update You Shortly !!!');
+            return redirect()->to(current_url());
+        }
         
         return view('delete-account-request', $data);
+    }
+
+    public function getEmailOTP(){
+        $apiStatus          = TRUE;
+        $apiMessage         = '';
+        $apiResponse        = [];
+        $requestData        = $this->request->getPost();
+        $user_type          = $requestData['user_type'];
+        $email              = $requestData['email'];
+        if($user_type == 'COMPANY'){
+            $tableName = 'ecoex_companies';
+        } else {
+            $tableName = 'ecomm_users';
+        }
+        $getEntity          = $this->common_model->find_data($tableName, 'row', ['email' => $email]);
+        if($getEntity){
+            $remember_token = rand(100000,999999);
+            /* send email */
+                $mailData                   = [
+                    'id'            => $getEntity->id,
+                    'email'         => $getEntity->email,
+                    'phone'         => $getEntity->phone,
+                    'otp'           => $remember_token,
+                ];
+                $generalSetting             = $this->common_model->find_data('general_settings', 'row');
+                $subject                    = $generalSetting->site_name.' :: Email Verify OTP For Signup';
+                $message                    = view('email-templates/otp',$mailData);
+                $this->sendMail($email, $subject, $message);
+
+                $apiResponse        = [
+                    'email_otp'     => $remember_token,
+                    'entity_name'   => (($getEntity)?$getEntity->company_name:''),
+                ];
+                $apiStatus          = TRUE;
+                $apiMessage         = 'OTP Sent To Email Successfully !!!';
+            /* send email */
+        } else {
+            $apiStatus          = FALSE;
+            $apiMessage         = 'We Don\'t Recognize You !!!';
+        }
+        $this->response_to_json($apiStatus, $apiMessage, $apiResponse);
+    }
+    public function getPhoneOTP(){
+        $apiStatus          = TRUE;
+        $apiMessage         = '';
+        $apiResponse        = [];
+        $requestData        = $this->request->getPost();
+        $user_type          = $requestData['user_type'];
+        $phone              = $requestData['phone'];
+        if($user_type == 'COMPANY'){
+            $tableName = 'ecoex_companies';
+        } else {
+            $tableName = 'ecomm_users';
+        }
+        $getEntity          = $this->common_model->find_data($tableName, 'row', ['phone' => $phone]);
+        if($getEntity){
+            $mobile_otp = rand(100000,999999);
+            /* send sms */
+                $message = "Dear ".(($getEntity)?$getEntity->company_name:'ECOEX').", ".$mobile_otp." is your verification OTP for registration at ECOEX PORTAL. Do not share this OTP with anyone for security reasons.";
+                $mobileNo = (($getEntity)?$getEntity->phone:'');
+                $this->sendSMS($mobileNo,$message);
+
+                $apiResponse        = [
+                    'phone_otp'     => $mobile_otp,
+                    'entity_name'   => (($getEntity)?$getEntity->company_name:''),
+                ];
+                $apiStatus          = TRUE;
+                $apiMessage         = 'OTP Sent To Phone Successfully !!!';
+            /* send sms */
+        } else {
+            $apiStatus          = FALSE;
+            $apiMessage         = 'We Don\'t Recognize You !!!';
+        }
+        $this->response_to_json($apiStatus, $apiMessage, $apiResponse);
     }
 }
--- app/Views/delete-account-request.php
+++ app/Views/delete-account-request.php
@@ -14,26 +14,43 @@ $this->common_model         = new CommonModel;
     <link href="<?=getenv('app.uploadsURL'.$general_settings->site_favicon)?>" rel="apple-touch-icon">
     <link rel="stylesheet" href="<?=getenv('app.adminAssetsURL')?>lightbox/lightbox.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-    .tablelook {
-        padding-top: 10px;
-        padding-bottom: 10px;
-        border: 1px solid #dddd;
-    }
-    ul.d-flex.whatimgs {
-        flex-wrap: wrap;
-        list-style: none;
-        padding: 0;
-    }
-    .whatimgs li {
-        margin: 5px;
-    }
-    .whatimgs img.example-image {
-        max-width: 60px;
-        border: 1px solid #999;
-        height: 60px;
-        width: 100%;
-    }
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet"/>
+    <style type="text/css">    
+        .toast-success {
+            background-color: #000;
+            color: #28a745 !important;
+        }
+        .toast-error {
+            background-color: #000;
+            color: #dc3545 !important;
+        }
+        .toast-warning {
+            background-color: #000;
+            color: #ffc107 !important;
+        }
+        .toast-info {
+            background-color: #000;
+            color: #007bff !important;
+        }
+        .tablelook {
+            padding-top: 10px;
+            padding-bottom: 10px;
+            border: 1px solid #dddd;
+        }
+        ul.d-flex.whatimgs {
+            flex-wrap: wrap;
+            list-style: none;
+            padding: 0;
+        }
+        .whatimgs li {
+            margin: 5px;
+        }
+        .whatimgs img.example-image {
+            max-width: 60px;
+            border: 1px solid #999;
+            height: 60px;
+            width: 100%;
+        }
     </style>
 </head>
 <body>
@@ -46,34 +63,58 @@ $this->common_model         = new CommonModel;
     <div class=" justify-content-center d-flex">
         <div class="row col-md-8 col-sm-12 col-xs-12">
             <form method="POST" action="" style="border: 1px solid #48974e73;padding: 15px;border-radius: 5px;">
+                <?php if(session('success_message')){?>
+                    <div class="alert alert-success bg-success text-light border-0 alert-dismissible fade show hide-message" role="alert">
+                        <?=session('success_message')?>
+                    </div>
+                <?php }?>
+                <?php if(session('error_message')){?>
+                    <div class="alert alert-danger bg-danger text-light border-0 alert-dismissible fade show hide-message" role="alert">
+                        <?=session('error_message')?>
+                    </div>
+                <?php }?>
+                <input type="hidden" name="entity_name" id="entity_name">
                 <div class="form-group mb-3">
-                    <label for="company_name" class="fw-bold">Company Name</label>
-                    <input type="text" class="form-control" id="company_name" name="company_name" placeholder="Company Name" required>
+                    <p class="fw-bold">Select User Type</p>
+                    <input type="radio" id="user_type1" name="user_type" value="COMPANY" checked required><label for="user_type1" style="margin-left: 5px; margin-right: 10px;">COMPANY</label>
+                    <input type="radio" id="user_type2" name="user_type" value="PLANT" required><label for="user_type2" style="margin-left: 5px; margin-right: 10px;">PLANT</label>
+                    <input type="radio" id="user_type3" name="user_type" value="VENDOR" required><label for="user_type3" style="margin-left: 5px; margin-right: 10px;">VENDOR</label>
                 </div>
                 <div class="form-group mb-3">
-                    <label for="Email" class="fw-bold">Email</label>
-                    <input type="text" class="form-control" id="Email" name="Email" placeholder="Email" required>
+                    <label for="email" class="fw-bold">Email</label>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+                    <button type="button" class="btn btn-primary btn-sm" id="email-otp-btn">Get Email OTP</button>
+                    <input type="hidden" name="generated_email_otp" id="generated_email_otp">
                 </div>
-                <div class="form-group mb-3">
-                    <label for="otp" class="fw-bold">OTP</label>
-                    <input type="text" class="form-control" id="otp" name="otp" placeholder="OTP" maxlength="4" required>
+                <div class="form-group mb-3" id="email_otp_row">
+                    <label for="email_otp" class="fw-bold">Email OTP</label>
+                    <input type="text" class="form-control" id="email_otp" name="email_otp" placeholder="Email OTP" maxlength="6" minlength="6" required onkeypress="return isNumber(event)">
+                    <button type="button" class="btn btn-info btn-sm" id="email-validate-btn">Validate Email OTP</button>
+                    <input type="hidden" name="is_email_verify" id="is_email_verify" value="0">
                 </div>
                 <div class="form-group mb-3">
                     <label for="phone" class="fw-bold">Phone Number</label>
-                    <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone Number" maxlength="10" minlength="10" required>
+                    <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone Number" maxlength="10" minlength="10" required onkeypress="return isNumber(event)">
+                    <button type="button" class="btn btn-primary btn-sm" id="phone-otp-btn">Get Phone OTP</button>
+                    <input type="hidden" name="generated_phone_otp" id="generated_phone_otp">
+                </div>
+                <div class="form-group mb-3" id="phone_otp_row">
+                    <label for="phone_otp" class="fw-bold">Phone OTP</label>
+                    <input type="text" class="form-control" id="phone_otp" name="phone_otp" placeholder="Phone OTP" maxlength="6" minlength="6" required onkeypress="return isNumber(event)">
+                    <button type="button" class="btn btn-info btn-sm" id="phone-validate-btn">Validate Email OTP</button>
+                    <input type="hidden" name="is_phone_verify" id="is_phone_verify" value="0">
                 </div>
                 <div class="form-group mb-3">
-                    <label for="address" class="fw-bold">Address</label>
-                    <textarea class="form-control" id="address" name="address" placeholder="Address" required></textarea>
+                    <label for="comments" class="fw-bold">Comments</label>
+                    <textarea class="form-control" id="comments" name="comments" placeholder="Comments" required></textarea>
                 </div>
                 <div class="form-group mb-3">
-                    <button type="submit" class="btn btn-success"><i class="fa fa-paper-plane"></i> Submit</button>
+                    <button type="submit" class="btn btn-success" id="submit-btn"><i class="fa fa-paper-plane"></i> Submit</button>
                 </div>
             </form>
         </div>
     </div>
-</div>  
-    
+</div>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>    
 <script src="<?=getenv('app.adminAssetsURL')?>lightbox/lightbox.js"></script>
@@ -81,6 +122,133 @@ $this->common_model         = new CommonModel;
     lightbox.option({
       'resizeDuration': 200,
       'wrapAround': true
+    })
+    function isNumber(evt) {
+        evt = (evt) ? evt : window.event;
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
+        }
+        return true;
+    }
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
+<script type="text/javascript">
+    function toastAlert(type, message, redirectStatus = false, redirectUrl = ''){
+        toastr.options = {
+            "closeButton": true,
+            "debug": true,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-bottom-left",
+            "preventDuplicates": false,
+            "showDuration": "3000",
+            "hideDuration": "1000000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+        toastr[type](message);
+        if(redirectStatus){        
+            setTimeout(function(){ window.location = redirectUrl; }, 3000);
+        }
+    }
+    $(function(){
+        $('#email_otp_row').hide();
+        $('#phone_otp_row').hide();
+        $('#submit-btn').attr('disabled', true);
+
+        $('#email-otp-btn').on('click', function(){
+            var user_type   = $('input[name="user_type"]:checked').val();
+            var email       = $('#email').val();
+            if(user_type != ''){
+                if(email != ''){
+                    let baseUrl = '<?=base_url()?>';
+                    $.ajax({
+                      type: "POST",
+                      data: { user_type: user_type, email: email },
+                      url: baseUrl+"/get-email-otp",
+                      dataType: "JSON",
+                      success: function(res){
+                        if(res.success){
+                            toastAlert('success', res.message);
+                            $('#entity_name').val(res.data.entity_name);
+                            $('#generated_email_otp').val(res.data.email_otp);
+                            $('#email_otp_row').show();
+                        } else {
+                            toastAlert('error', res.message);
+                        }
+                      }
+                    });
+                } else {
+                    toastAlert('error', 'Please Enter Email !!!');
+                }
+            } else {
+                toastAlert('error', 'Please Select User Type !!!');
+            }
+        });
+        $('#email-validate-btn').on('click', function(){
+            var generated_email_otp         = parseInt($('#generated_email_otp').val());
+            var email_otp                   = parseInt($('#email_otp').val());
+            if(generated_email_otp == email_otp){
+                toastAlert('success', 'Email OTP Validated !!!');
+                $('#is_email_verify').val(1);
+                if($('#is_phone_verify').val() == 1){
+                    $('#submit-btn').attr('disabled', false);
+                }
+            } else {
+                toastAlert('error', 'Email OTP Mismatched !!!');
+                $('#email_otp').val('');
+            }
+        });
+
+        $('#phone-otp-btn').on('click', function(){
+            var user_type   = $('input[name="user_type"]:checked').val();
+            var phone       = $('#phone').val();
+            if(user_type != ''){
+                if(phone != ''){
+                    let baseUrl = '<?=base_url()?>';
+                    $.ajax({
+                      type: "POST",
+                      data: { user_type: user_type, phone: phone },
+                      url: baseUrl+"/get-phone-otp",
+                      dataType: "JSON",
+                      success: function(res){
+                        if(res.success){
+                            toastAlert('success', res.message);
+                            $('#entity_name').val(res.data.entity_name);
+                            $('#generated_phone_otp').val(res.data.phone_otp);
+                            $('#phone_otp_row').show();
+                        } else {
+                            toastAlert('error', res.message);
+                        }
+                      }
+                    });
+                } else {
+                    toastAlert('error', 'Please Enter Phone No. !!!');
+                }
+            } else {
+                toastAlert('error', 'Please Select User Type !!!');
+            }
+        });
+        $('#phone-validate-btn').on('click', function(){
+            var generated_phone_otp         = parseInt($('#generated_phone_otp').val());
+            var phone_otp                   = parseInt($('#phone_otp').val());
+            if(generated_phone_otp == phone_otp){
+                toastAlert('success', 'Phone OTP Validated !!!');
+                $('#is_phone_verify').val(1);
+                if($('#is_email_verify').val() == 1){
+                    $('#submit-btn').attr('disabled', false);
+                }
+            } else {
+                toastAlert('error', 'Phone OTP Mismatched !!!');
+                $('#phone_otp').val('');
+            }
+        });
+
     })
 </script>
 </body>
