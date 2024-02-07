cb1e8d622d0a3c25e7e178f9be6eef9d183f0279
subhomoy samanta <subhomoy@keylines.net>
2024-02-07 18:04:54 +0530

    email setting update

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -144,8 +144,8 @@ abstract class BaseController extends Controller
             $emailSetting->protocol = 'sendmail';
             $emailSetting->setFrom($from_email, $from_name);
             $emailSetting->setTo($to_email);
-            $emailSetting->setBCC('enquiry@commodity.ecoex.market', 'Ecoex Commodity Trading');
-            $emailSetting->setCC('subhomoysamanta1989@gmail.com', 'Ecoex Commodity Trading');
+            $emailSetting->setBCC('sudip.keyline@gmail.com', 'Ecoex Commodity Trading');
+            $emailSetting->setCC('subhomoy@keylines.net', 'Ecoex Commodity Trading');
             $emailSetting->setSubject($email_subject);
             $emailSetting->setMessage($mailbody);
 
