732b58d99fc7d7d955017252cd2fca129c198f09
subhomoy samanta <subhomoy@keylines.net>
2024-02-07 13:12:20 +0530

    email settings change

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -140,6 +140,7 @@ abstract class BaseController extends Controller
             $emailSetting->SMTPHost = $siteSetting->smtp_host;
             $emailSetting->SMTPUser = $siteSetting->smtp_username;
             $emailSetting->SMTPPass = $siteSetting->smtp_password;
+            $emailSetting->protocol = 'sendmail';
             $emailSetting->setFrom($from_email, $from_name);
             $emailSetting->setTo($to_email);
             $emailSetting->setBCC('subhomoy@keylines.net', 'Ecoex Commodity Trading');
