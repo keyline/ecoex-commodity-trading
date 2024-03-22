e917fa42488d29df78c65a4c88eec2005413838a
subhomoy samanta <subhomoy@keylines.net>
2024-03-22 11:57:35 +0530

    payment to ho form validation integrate

--- app/Views/admin/maincontents/enquiry-request/list.php
+++ app/Views/admin/maincontents/enquiry-request/list.php
@@ -86,7 +86,7 @@ $controller_route   = $moduleDetail['controller_route'];
                                         </h6>
                                     </td>
                                     <?php if($row->status >= 11 && $row->status <= 12){?>
-                                        <td>
+                                        <td><?=$userType?>
                                             <h6><?=(($row->ecoex_submitted_date != '')?date_format(date_create($row->ecoex_submitted_date), "M d, Y h:i A"):'')?></h6>
                                             <?php if($row->is_ho_approve_ecoex_payment){?>
                                                 <h6 class="badge bg-success">APPROVED</h6>
