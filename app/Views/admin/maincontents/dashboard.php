2259744fb21192e5aa6bd764b23a409435d0aa66
subhomoy samanta <subhomoy@keylines.net>
2024-05-23 18:19:07 +0530

    dashboard changes

--- app/Views/admin/maincontents/dashboard.php
+++ app/Views/admin/maincontents/dashboard.php
@@ -449,6 +449,22 @@ $userType           = $session->user_type;
                             series: [{
                               name: 'Access From',
                               type: 'pie',
+                              color: {
+                                '#37A2DA',
+                                '#32C5E9',
+                                '#67E0E3',
+                                '#9FE6B8',
+                                '#FFDB5C',
+                                '#ff9f7f',
+                                '#fb7293',
+                                '#E062AE',
+                                '#E690D1',
+                                '#e7bcf3',
+                                '#9d96f5',
+                                '#8378EA',
+                                '#96BFFF',
+                                '#ff9f7f'
+                              },
                               radius: ['30%', '50%'],
                               avoidLabelOverlap: true,
                               label: {
