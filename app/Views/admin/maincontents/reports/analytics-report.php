543f7b32eff1071060549a0d822d10d5907a76de
debojyti <debojyti@keylines.net>
2024-06-03 16:26:35 +0530

    mis report implemented 03-06-2024

--- app/Views/admin/maincontents/reports/analytics-report.php
+++ app/Views/admin/maincontents/reports/analytics-report.php
@@ -76,9 +76,9 @@ $controller_route   = $moduleDetail['controller_route'];
                 <div class="col-md-4 col-lg-4" id="day_range_row" style="display: <?=(($is_date_range == 1)?'block':'none')?>; margin-top: 18px;">
                     <div class="input-group input-daterange">
                         <!-- <label for="search_range_from">Date Range</label> -->
-                        <input type="date" id="search_range_from" name="search_range_from" class="form-control" value="<?=$search_range_from?>" style="height: 40px;">
+                        <input type="month" id="search_range_from" name="search_range_from" class="form-control" value="<?=$search_range_from?>" style="height: 40px;">
                         <span class="input-group-text">To</span>
-                        <input type="date" id="search_range_to" name="search_range_to" class="form-control" value="<?=$search_range_to?>" max="<?=date('Y-m-d')?>" style="height: 40px;">
+                        <input type="month" id="search_range_to" name="search_range_to" class="form-control" value="<?=$search_range_to?>" max="" style="height: 40px;">
                     </div>
                 </div>
             </div>
