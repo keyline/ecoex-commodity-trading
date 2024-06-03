989a9544ee893bb701fe44d11ad41be74f19ec13
debojyti <debojyti@keylines.net>
2024-06-03 16:13:52 +0530

    mis report implemented 03-06-2024

--- app/Views/admin/maincontents/reports/analytics-report.php
+++ app/Views/admin/maincontents/reports/analytics-report.php
@@ -88,7 +88,77 @@ $controller_route   = $moduleDetail['controller_route'];
                     <a href="<?=base_url('admin/reports/advance-search')?>" class="btn btn-secondary"><i class="fa fa-refresh"></i> Reset</a>
                 <?php }?>
             </div>
-        </form>
+          </form>
+
+          <div class="row">
+            <div class="col-lg-12 col-md-12">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Column Chart</h5>
+
+                  <!-- Column Chart -->
+                  <div id="columnChart"></div>
+
+                  <script>
+                    document.addEventListener("DOMContentLoaded", () => {
+                      new ApexCharts(document.querySelector("#columnChart"), {
+                        series: [{
+                          name: 'Net Profit',
+                          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+                        }, {
+                          name: 'Revenue',
+                          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+                        }, {
+                          name: 'Free Cash Flow',
+                          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+                        }],
+                        chart: {
+                          type: 'bar',
+                          height: 350
+                        },
+                        plotOptions: {
+                          bar: {
+                            horizontal: false,
+                            columnWidth: '55%',
+                            endingShape: 'rounded'
+                          },
+                        },
+                        dataLabels: {
+                          enabled: false
+                        },
+                        stroke: {
+                          show: true,
+                          width: 2,
+                          colors: ['transparent']
+                        },
+                        xaxis: {
+                          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+                        },
+                        yaxis: {
+                          title: {
+                            text: '$ (thousands)'
+                          }
+                        },
+                        fill: {
+                          opacity: 1
+                        },
+                        tooltip: {
+                          y: {
+                            formatter: function(val) {
+                              return "$ " + val + " thousands"
+                            }
+                          }
+                        }
+                      }).render();
+                    });
+                  </script>
+                  <!-- End Column Chart -->
+
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
