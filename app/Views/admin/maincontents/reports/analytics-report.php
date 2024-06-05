59a1c37e760fe825a77748f7cc721ce8b7ac2827
subhomoy samanta <subhomoy@keylines.net>
2024-06-05 12:46:22 +0530

    mis report implemented 05-06-2024

--- app/Views/admin/maincontents/reports/analytics-report.php
+++ app/Views/admin/maincontents/reports/analytics-report.php
@@ -85,7 +85,7 @@ $controller_route   = $moduleDetail['controller_route'];
             <div class="text-center">
                 <button type="submit" class="btn btn-primary"><i class="fa fa-paper-plane"></i> Generate</button>
                 <?php if(!empty($is_search)){?>
-                    <a href="<?=base_url('admin/reports/advance-search')?>" class="btn btn-secondary"><i class="fa fa-refresh"></i> Reset</a>
+                    <a href="<?=base_url('admin/reports/analytics-report')?>" class="btn btn-secondary"><i class="fa fa-refresh"></i> Reset</a>
                 <?php }?>
             </div>
           </form>
