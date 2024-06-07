5c9d945ece66a458c65b1f675b2c1db7f522a7f5
subhomoy samanta <subhomoy@keylines.net>
2024-06-07 14:27:04 +0530

    mis report 07-06-2024

--- app/Views/admin/maincontents/reports/analytics-report.php
+++ app/Views/admin/maincontents/reports/analytics-report.php
@@ -233,10 +233,10 @@ $controller_route   = $moduleDetail['controller_route'];
             }
             let html = '<option value="all" ' + selected + '>All</option><hr>';
             $.each(rply.data, function(key, item) {
-              if(search_product_id == item.name){
+              if(search_product_id == item.id){
                 selected = 'selected';
               }
-              html += '<option value="' + item.name + '' + selected + '">' + item.name + ' (' + item.unit + ')</option><hr>';
+              html += '<option value="' + item.id + '' + selected + '">' + item.name + ' (' + item.unit + ')</option><hr>';
             });
             $("#search_product_id").html(html);
           }else{
