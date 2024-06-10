c7617eb800a394bf047c1db42dad91ea01d7ee4e
subhomoy samanta <subhomoy@keylines.net>
2024-06-10 12:54:01 +0530

    mis report updated 10-06-2024

--- app/Controllers/Admin/ReportController.php
+++ app/Controllers/Admin/ReportController.php
@@ -96,7 +96,7 @@ class ReportController extends BaseController {
                     if($enquires){
                         foreach($enquires as $enquiry){
                             if($search_product_id == 'all'){
-                                $subEnquiries = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['enq_id' => $enquiry->id, 'weighted_unit' => $search_unit_id], 'vehicle_registration_nos,weighted_qty,item_id,weighted_unit,enquiry_no,sub_enquiry_no');
+                                $subEnquiries = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['enq_id' => $enquiry->id, 'weighted_unit' => $search_unit_id], 'vehicle_registration_nos,weighted_qty,item_id,weighted_unit,enquiry_no,sub_enquiry_no,enq_id');
                             } else {
                                 $subEnquiries = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['enq_id' => $enquiry->id, 'weighted_unit' => $search_unit_id, 'item_id' => $search_product_id], 'vehicle_registration_nos,weighted_qty,item_id,weighted_unit,enquiry_no,sub_enquiry_no,enq_id');
                             }
