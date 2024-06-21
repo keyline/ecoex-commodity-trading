217342e9035e585cc0625595a9ed275aeb0a8752
subhomoy samanta <subhomoy@keylines.net>
2024-06-21 13:26:56 +0530

    mis report updated 21-06-2024

--- app/Controllers/Admin/ReportController.php
+++ app/Controllers/Admin/ReportController.php
@@ -128,8 +128,11 @@ class ReportController extends BaseController {
                                             $weightMatQty[]         = $subEnquiry->weighted_qty;
                                             $convertedUnit          = $search_unit_id;
                                         } else {
-                                            // $weightMatQty[]         = weightConversion($subEnquiry->weighted_qty, 'KG', 'MT');
-                                            $weightMatQty[]         = $subEnquiry->weighted_qty;
+                                            if($search_unit_id == 'MT'){
+                                                $weightMatQty[]         = weightConversion($subEnquiry->weighted_qty, 'KG', 'MT');
+                                            } else {
+                                                $weightMatQty[]         = $subEnquiry->weighted_qty;
+                                            }
                                             $convertedUnit          = $search_unit_id;
                                         }
                                     }
