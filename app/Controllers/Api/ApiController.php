bd99330dc583097e65771ccca33cdf1054b00f5f
subhomoy samanta <subhomoy@keylines.net>
2025-08-04 20:06:52 +0530

    price list api implemented

--- app/Controllers/Api/ApiController.php
+++ app/Controllers/Api/ApiController.php
@@ -229,8 +229,8 @@ class ApiController extends BaseController
                 $top_prices         = [];
 
                 if($state == 'all'){
-                    $groupBy[0] = 'ecomm_sub_enquires.product_id';
-                    $join['0']  = ['table' => 'ecomm_company_items', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'product_id', 'type' => 'INNER'];
+                    $groupBy[0] = 'ecomm_sub_enquires.item_id';
+                    $join['0']  = ['table' => 'ecomm_company_items', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'item_id', 'type' => 'INNER'];
                     $getEnquiryItems = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['ecomm_sub_enquires.created_at>=' => $startOfLastWeek, 'ecomm_sub_enquires.created_at<=' => $endOfLastWeek], 'ecomm_sub_enquires.sub_enquiry_no, ecomm_sub_enquires.item_id, ecomm_company_items.item_name_ecoex', $join, $groupBy);
                     pr($getEnquiryItems);
                 } else {
