91960c20ce3f70f60920fde70108966e55805b73
subhomoy samanta <subhomoy@keylines.net>
2025-08-06 13:33:45 +0530

    certificate upload task implement

--- app/Views/admin/maincontents/company/manage-certificate.php
+++ app/Views/admin/maincontents/company/manage-certificate.php
@@ -40,7 +40,7 @@ $userType           = $session->user_type;
                                 </div>
                                  <div class="col-md-2">
                                     <h5 class="card-titles mb-3">
-                                        <a href="<?=base_url('admin/companies/upload-certificate')?>" class="btn btn-ouline-success btn-sm">Upload New Certificates</a>
+                                        <a href="<?=base_url('admin/companies/upload-certificate')?>" class="btn btn-outline-success">Upload New Certificates</a>
                                     </h5>
                                 </div>
                             </div>
