1762a12f4c7d99a10266e24476ac66b90b2a0d18
subhomoy samanta <subhomoy@keylines.net>
2025-08-06 14:58:10 +0530

    certificate upload task implement

--- app/Controllers/Admin/CompanyController.php
+++ app/Controllers/Admin/CompanyController.php
@@ -785,7 +785,7 @@ class CompanyController extends BaseController {
             $data['company_name']       = $company_name;
 
             $orderBy[0]                 = ['field' => 'id', 'type' => 'DESC'];
-            $data['certificates']       = $this->common_model->find_data('ecomm_company_certificates', 'array', ['company_id' => $id, 'status' => 1], 'certificate_file,created_at', '', '', $orderBy);
+            $data['certificates']       = $this->common_model->find_data('ecomm_company_certificates', 'array', ['company_id' => $id, 'status' => 1], 'certificate_file,created_at,filename', '', '', $orderBy);
                   
             echo $this->layout_after_login($title,$page_name,$data);
         }
