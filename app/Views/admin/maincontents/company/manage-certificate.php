de7030cefdfbdfab00f00dd5cfd1ae2bb6ba2773
subhomoy samanta <subhomoy@keylines.net>
2025-08-06 13:17:25 +0530

    certificate upload task implement

--- app/Views/admin/maincontents/company/manage-certificate.php
+++ app/Views/admin/maincontents/company/manage-certificate.php
@@ -17,25 +17,159 @@ $userType           = $session->user_type;
     </div>
 </div>
 
-<!-- End Page Title -->
-<section class="section profile">
+<section class="section">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xl-12">
-                <?php if(session('success_message')){?>
-                <div class="alert alert-success bg-success text-light border-0 alert-dismissible fade show hide-message" role="alert">
-                    <?=session('success_message')?>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="container py-4">
+                            <div class="row justify-content-between">
+                                <div class="col-md-6">
+                                    <h5 class="card-titles mb-3">
+                                        <strong>Nourishco Certificates</strong>
+                                    </h5>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="input-group shadow-sm rounded-pill" style="overflow: hidden; max-width: 300px;float: right;">
+                                      <input type="text" class="form-control border-0" placeholder="Search here" aria-label="Search">
+                                      <span class="input-group-text bg-white border-0">
+                                        <i class="bi bi-search"></i>
+                                      </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="container py-4">
+                          <div class="row g-4">
+
+                            <!-- Repeated Card -->
+                            <div class="col-md-4">
+                              <div class="card card-custom p-3">
+                                <div class="card-body">
+                                  <h6 class="text-muted mb-2 border-bottom">Parsva Packers Limited</h6>
+                                  <h5 class="fw-bold mb-0">S1938383883</h5>
+                                  <div class="text-muted">Invoice No</div>
+                                  <h5 class="fw-bold mt-3 mb-0">128283</h5>
+                                  <div class="text-muted">Certificate No</div>
+                                  <div class="d-flex justify-content-between align-items-center mt-4">
+                                    <a href="#" class="text-decoration-none text-viewcolor">View</a>
+                                    <button class="btn btn-download download-icon px-3">Download</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
+                            <!-- Repeat 4x -->
+                            <div class="col-md-4">
+                              <div class="card card-custom p-3">
+                                <div class="card-body">
+                                  <h6 class="text-muted mb-2 border-bottom">Parsva Packers Limited</h6>
+                                  <h5 class="fw-bold mb-0">S1938383883</h5>
+                                  <div class="text-muted">Invoice No</div>
+                                  <h5 class="fw-bold mt-3 mb-0">128283</h5>
+                                  <div class="text-muted">Certificate No</div>
+                                  <div class="d-flex justify-content-between align-items-center mt-4">
+                                    <a href="#" class="text-decoration-none text-viewcolor">View</a>
+                                    <button class="btn btn-download download-icon px-3">Download</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
+                            <div class="col-md-4">
+                              <div class="card card-custom p-3">
+                                <div class="card-body">
+                                  <h6 class="text-muted mb-2 border-bottom">Parsva Packers Limited</h6>
+                                  <h5 class="fw-bold mb-0">S1938383883</h5>
+                                  <div class="text-muted">Invoice No</div>
+                                  <h5 class="fw-bold mt-3 mb-0">128283</h5>
+                                  <div class="text-muted">Certificate No</div>
+                                  <div class="d-flex justify-content-between align-items-center mt-4">
+                                    <a href="#" class="text-decoration-none text-viewcolor">View</a>
+                                    <button class="btn btn-download download-icon px-3">Download</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
+                            <div class="col-md-4">
+                              <div class="card card-custom p-3">
+                                <div class="card-body">
+                                  <h6 class="text-muted mb-2 border-bottom">Parsva Packers Limited</h6>
+                                  <h5 class="fw-bold mb-0">S1938383883</h5>
+                                  <div class="text-muted">Invoice No</div>
+                                  <h5 class="fw-bold mt-3 mb-0">128283</h5>
+                                  <div class="text-muted">Certificate No</div>
+                                  <div class="d-flex justify-content-between align-items-center mt-4">
+                                    <a href="#" class="text-decoration-none text-viewcolor">View</a>
+                                    <button class="btn btn-download download-icon px-3">Download</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
+                            <!-- Empty Card -->
+                            <div class="col-md-4">
+                              <div class="card card-custom card-empty">
+                                <div class="card-body">
+                                    <button class="btn btn-download mb-2">Download Zip File</button>
+                                    <div class="text-muted">View Zip File</div>
+                                </div>
+                              </div>
+                            </div>
+
+                          </div>
+                        </div>
+                    </div>
                 </div>
-                <?php }?>
-                <?php if(session('error_message')){?>
-                <div class="alert alert-danger bg-danger text-light border-0 alert-dismissible fade show hide-message" role="alert">
-                    <?=session('error_message')?>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-                <?php }?>
             </div>
-                        
         </div>
     </div>
+    
+    <!-- Recent Sales -->
+    <!-- End Recent Sales -->
 </section>
+<style>
+ .border-bottom {
+    border-bottom: 1px solid #e0e0e0 !important;
+  }
+.card-custom {
+  box-shadow: 0 4px 20px rgba(0,0,0,0.05);
+  border: none;
+  border-radius: 12px;
+  transition: 0.3s;
+    min-height: 260px;
+}
+.btn-download {
+  background-color: #198754;
+  color: #fff;
+  border-radius: 6px;
+}
+.btn-download:hover {
+  background-color: #00795f;
+  color: #fff;
+}
+.text-viewcolor{
+   color: #198754; 
+}
+.card-body {
+  font-size: 14px;
+}
+.download-icon::after {
+  content: "➔";
+  margin-left: 6px;
+}
+.card-empty {
+  background-color: #fff;
+  text-align: center;
+  padding: 2rem 1rem;
+}
+.card-empty .card-body {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
