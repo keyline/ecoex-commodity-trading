53a8cb40737cffa6c6740c8680c3485cdd047400
subhomoy samanta <subhomoy@keylines.net>
2025-08-06 13:10:16 +0530

    certificate upload task implement

--- app/Views/admin/maincontents/company/list.php
+++ app/Views/admin/maincontents/company/list.php
@@ -140,7 +140,7 @@ $company_id                 = $session->company_id;
                                                         <a href="<?= base_url('admin/' . $controller_route . '/manage-item/' . encoded($row->$primary_key)) ?>" class="btn btn-info btn-sm mt-1" title="Manage Item"><i class="fa fa-tasks"></i> Items <?= $assignItemText ?></a>
                                                         <br>
                                                     <?php } ?>
-                                                    <br><br>
+                                                    <br>
                                                     <?php
                                                     $certficate_count = $common_model->find_data('ecomm_company_certificates', 'count', ['company_id' => $row->$primary_key, 'status!=' => 3]);
                                                     ?>
