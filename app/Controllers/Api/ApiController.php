125b646e61bacb99950c426a659a2a02eb9adb0f
subhomoy samanta <subhomoy@keylines.net>
2025-08-20 13:38:12 +0530

    price list api updated

--- app/Controllers/Api/ApiController.php
+++ app/Controllers/Api/ApiController.php
@@ -262,7 +262,8 @@ class ApiController extends BaseController
             if (empty($getSubEnquiryItems)) {
                 $groupBy[0] = 'ecomm_sub_enquires.item_id';
                 $join['0']  = ['table' => 'ecomm_company_items', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'item_id', 'type' => 'INNER'];
-                $getSubEnquiryItems = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['ecomm_sub_enquires.assigned_date>=' => $startOfLastWeek, 'ecomm_sub_enquires.assigned_date<=' => $endOfLastWeek], 'ecomm_sub_enquires.sub_enquiry_no, ecomm_sub_enquires.item_id, ecomm_company_items.item_name_ecoex', $join, $groupBy);
+                // $getSubEnquiryItems = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['ecomm_sub_enquires.assigned_date>=' => $startOfLastWeek, 'ecomm_sub_enquires.assigned_date<=' => $endOfLastWeek], 'ecomm_sub_enquires.sub_enquiry_no, ecomm_sub_enquires.item_id, ecomm_company_items.item_name_ecoex', $join, $groupBy);
+                $getSubEnquiryItems = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['ecoex_companies.state' => $state], 'ecomm_sub_enquires.sub_enquiry_no, ecomm_sub_enquires.item_id, ecomm_company_items.item_name_ecoex', $join, $groupBy);
             }
 
             $total_win_price_array = [];
