249877100c437c6a8afecc5e3c7ab1d6c25fb19f
subhomoy samanta <subhomoy@keylines.net>
2025-08-25 15:52:17 +0530

    price api updated

--- app/Controllers/Api/ApiController.php
+++ app/Controllers/Api/ApiController.php
@@ -331,7 +331,7 @@ class ApiController extends BaseController
                         'tot_item_win_price_before'     => number_format($before_price, 2),
                         'difference_price'              => number_format($difference_price, 2),
                         'comparison_stat'               => (($difference_price >= 0) ? '+' : '-'),
-                        'comparison_percentage'         => abs(number_format((($before_price > 0) ? (($difference_price / $before_price) * 100) : ($difference_price)), 2)),
+                        'comparison_percentage'         => abs((($before_price > 0) ? (($difference_price / $before_price) * 100) : ($difference_price))),
                     ];
                 }
             }
