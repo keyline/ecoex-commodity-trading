51fed9224d64c41e4355e2193cd9468ab7210d0c
subhomoy samanta <subhomoy@keylines.net>
2025-08-27 16:42:48 +0530

    company report show in company admin

--- app/Controllers/Admin/ReportController.php
+++ app/Controllers/Admin/ReportController.php
@@ -257,13 +257,12 @@ class ReportController extends BaseController
     public function companyReport()
     {
         $user_type                          = session('user_type');
-        echo $company_id                         = session('company_id');
+        $company_id                         = session('company_id');
         $title                              = 'Manage Company Reports';
         $page_name                          = 'reports/plants-report';
 
         if($user_type == 'COMPANY'){
-            $data['companies']  = $this->common_model->find_data('ecoex_companies', 'result-array', ['status' => 1, 'id' => $company_id], 'id,company_name');
-            pr($data['companies']);
+            $data['companies']  = $this->common_model->find_data('ecoex_companies', 'result-array', ['status>=' => 2, 'id' => $company_id], 'id,company_name');
         } else {
             $data['companies']                  = $this->plantService->getCompanies();
         }
