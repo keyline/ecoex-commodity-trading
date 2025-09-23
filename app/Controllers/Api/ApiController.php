2469460c7c687813d250e6fde2a669fe83a4cc6f
subhomoy samanta <subhomoy@keylines.net>
2025-09-23 12:30:55 +0530

    price list api update

--- app/Controllers/Api/ApiController.php
+++ app/Controllers/Api/ApiController.php
@@ -248,27 +248,27 @@ class ApiController extends BaseController
 
             /* top prices */
             $top_prices         = [];
-            if ($state == 'All') {
+            if ($state == 'all') {
                 $groupBy[0] = 'ecomm_sub_enquires.item_id';
                 $join['0']  = ['table' => 'ecomm_company_items', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'item_id', 'type' => 'INNER'];
-                $getSubEnquiryItems = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['ecomm_sub_enquires.assigned_date>=' => $startOfLastLastWeek, 'ecomm_sub_enquires.assigned_date<=' => $endOfLastLastWeek], 'ecomm_sub_enquires.sub_enquiry_no, ecomm_sub_enquires.item_id, ecomm_company_items.item_name_ecoex', $join, $groupBy);
+                $getSubEnquiryItems = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['ecomm_sub_enquires.assigned_date>=' => $startOfLastWeek, 'ecomm_sub_enquires.assigned_date<=' => $endOfLastWeek], 'ecomm_sub_enquires.sub_enquiry_no, ecomm_sub_enquires.item_id, ecomm_company_items.item_name_ecoex', $join, $groupBy);
             } else {
                 $groupBy[0] = 'ecomm_sub_enquires.item_id';
                 $join3['0']  = ['table' => 'ecomm_company_items', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'item_id', 'type' => 'INNER'];
                 $join3['1']  = ['table' => 'ecoex_companies', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'company_id', 'type' => 'INNER'];
-                $getSubEnquiryItems = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['ecomm_sub_enquires.assigned_date>=' => $startOfLastLastWeek, 'ecomm_sub_enquires.assigned_date<=' => $endOfLastLastWeek, 'ecoex_companies.state' => $state], 'ecomm_sub_enquires.sub_enquiry_no, ecomm_sub_enquires.item_id, ecomm_company_items.item_name_ecoex', $join3, $groupBy);
+                $getSubEnquiryItems = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['ecomm_sub_enquires.assigned_date>=' => $startOfLastWeek, 'ecomm_sub_enquires.assigned_date<=' => $endOfLastWeek, 'ecoex_companies.state' => $state], 'ecomm_sub_enquires.sub_enquiry_no, ecomm_sub_enquires.item_id, ecomm_company_items.item_name_ecoex', $join3, $groupBy);
             }
 
-            $this->db = \Config\Database::connect();
-            echo $this->db->getLastQuery();
-            die;
+            // $this->db = \Config\Database::connect();
+            // echo $this->db->getLastQuery();
+            // die;
 
             if (empty($getSubEnquiryItems)) {
                 // $groupBy[0] = 'ecomm_sub_enquires.item_id';
                 // $join4['0']  = ['table' => 'ecomm_company_items', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'item_id', 'type' => 'INNER'];
                 // $join4['1']  = ['table' => 'ecoex_companies', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'company_id', 'type' => 'INNER'];
                 // $getSubEnquiryItems = $this->common_model->find_data('ecomm_sub_enquires', 'array', ['ecomm_sub_enquires.assigned_date>=' => $startOfLastWeek, 'ecomm_sub_enquires.assigned_date<=' => $endOfLastWeek], 'ecomm_sub_enquires.sub_enquiry_no, ecomm_sub_enquires.item_id, ecomm_company_items.item_name_ecoex', $join4, $groupBy);
-                if($state != 'All'){
+                if($state != 'all'){
                     $groupBy[0] = 'ecomm_sub_enquires.item_id';
                     $join11['0']  = ['table' => 'ecomm_company_items', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'item_id', 'type' => 'INNER'];
                     $join11['1']  = ['table' => 'ecoex_companies', 'field' => 'id', 'table_master' => 'ecomm_sub_enquires', 'field_table_master' => 'company_id', 'type' => 'INNER'];
