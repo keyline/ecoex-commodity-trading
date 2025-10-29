1189215e64abb522bee1d79abdfa04a751ae3b25
subhomoy samanta <subhomoy@keylines.net>
2025-10-29 11:10:05 +0530

    quotation new api updated

--- app/Controllers/Api/ApiController.php
+++ app/Controllers/Api/ApiController.php
@@ -735,7 +735,7 @@ class ApiController extends BaseController
             if($selected_scrap != ''){
                 $requestNotSubmittedEnquiries = $this->common_model->find_data('ecomm_enquires', 'array', ['ecomm_enquires.status<=' => 12, 'ecomm_company_items.item_name_ecoex' => $selected_scrap], 'ecomm_enquires.plant_id,ecomm_users.state', $join, $groupBy, $orderBy);
             } else {
-                $requestNotSubmittedEnquiries = $this->common_model->find_data('ecomm_enquires', 'array', ['ecomm_enquires.status<=' => 12], 'ecomm_enquires.plant_id,ecomm_users.state', $join, $groupBy, $orderBy);
+                $requestNotSubmittedEnquiries = $this->common_model->find_data('ecomm_enquires', 'array', ['ecomm_enquires.status<=' => 6], 'ecomm_enquires.plant_id,ecomm_users.state', $join, $groupBy, $orderBy);
             }
 
             $response = [];
