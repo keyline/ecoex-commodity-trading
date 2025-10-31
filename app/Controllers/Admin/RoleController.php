8d7b7ad5c90d02dc408ad4e4da0e81c3b185d0e5
shuvadeep Chakraborty <shuvadeep@keylines.net>
2025-10-31 12:29:21 +0530

    role module updated for plants assign

--- app/Controllers/Admin/RoleController.php
+++ app/Controllers/Admin/RoleController.php
@@ -160,7 +160,13 @@ class RoleController extends BaseController {
         $data['plantLists']             = $this->common_model->find_data('ecomm_users', 'array', ['type' => 'PLANT', 'status>=' => 1, 'status<=' => 2], 'id,plant_name,full_address,state', '', '', $orderBy);
 
         if($this->request->getMethod() == 'post') {
-            pr($this->request->getPost());
+            echo $id;
+            $fields = [
+                'plant_ids'             => $this->request->getPost('plant_ids_json'),
+            ];
+            pr($fields);
+            // $this->common_model->save_data('ecoex_roles', $fields, $id, 'id');
+            // pr($this->request->getPost());
             
             $this->session->setFlashdata('success_message', $this->data['title'].' updated successfully');
             return redirect()->to('/admin/'.$this->data['controller_route'].'/list');
