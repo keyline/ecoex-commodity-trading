72e8e73ee0726010f08055595ab5d69f5b97c46c
subhomoy samanta <subhomoy@keylines.net>
2025-11-12 13:44:28 +0530

    enquiry module updated for company account

--- app/Controllers/Admin/EnquiryRequestController.php
+++ app/Controllers/Admin/EnquiryRequestController.php
@@ -100,8 +100,7 @@ class EnquiryRequestController extends BaseController
         $user_id                    = session('user_id');
         $getAdminUser               = $this->data['model']->find_data('ecoex_admin_user', 'row', ['id' => $user_id], 'plant_ids');
         $plantIds                   = (($getAdminUser)?json_decode($getAdminUser->plant_ids):[]);
-        pr($plantIds);
-
+        
         if(!empty($plantIds)){
             // Filter the array
             $filtered = array_filter($bugArray, function($item) use ($plantIds) {
