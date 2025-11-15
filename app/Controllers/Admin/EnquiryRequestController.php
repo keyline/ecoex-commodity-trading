24b3feb4027f09879a79ab0c2145fa65be8dec81
subhomoy samanta <subhomoy@keylines.net>
2025-11-15 08:20:21 +0530

    enquiry details bug fixing

--- app/Controllers/Admin/EnquiryRequestController.php
+++ app/Controllers/Admin/EnquiryRequestController.php
@@ -194,7 +194,7 @@ class EnquiryRequestController extends BaseController
 
         $order_by[0]                = array('field' => 'company_name', 'type' => 'asc');
         $data['avlVendors']         = $this->data['model']->find_data('ecomm_users', 'array', ['type' => 'VENDOR', 'status>=' => 1], 'id,company_name', '', '', $order_by);
-
+        pr($data['avlVendors']);
         $data['sharedVendors']      = $this->common_model->find_data('ecomm_enquiry_vendor_shares', 'array', ['enq_id' => $enq_id]);
 
         if ($this->request->getMethod() == 'post') {
