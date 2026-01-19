1609fa5bcc5ce2b8e22b90effcf5851aeedc4814
AnirbanKeyline <anirban.singh@keylines.net>
2026-01-19 15:17:58 +0530

    wp api testing

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -580,14 +580,14 @@ class Home extends BaseController
 
     public function wpMessage()
     {
-        $db = \Config\Database::connect();
+        // $db = \Config\Database::connect();
 
         // dd('hehe');
 
         $data['general_settings']   = $this->common_model->find_data('general_settings', 'row');
         $data['title']              = 'Wp Message - ' . $data['general_settings']->site_name;
-        $data['page_header']        = 'wp-message';
-        return view('analytics', $data);
+        $data['page_header']        = 'Wp Message';
+        return view('wp-message', $data);
     }
 
 
