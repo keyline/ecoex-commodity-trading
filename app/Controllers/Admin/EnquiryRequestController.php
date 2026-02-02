02dfa39b192e58d23d99dd9a78c80e5df61fec38
AnirbanKeyline <anirban.singh@keylines.net>
2026-02-02 18:03:43 +0530

    wp api modifications

--- app/Controllers/Admin/EnquiryRequestController.php
+++ app/Controllers/Admin/EnquiryRequestController.php
@@ -267,7 +267,7 @@ class EnquiryRequestController extends BaseController
     }
     public function send_whatsapp_campaign($username, $phone, $image, $param2, $param3, $param4, $param5)
     {
-        pr($image);die;
+        // pr($image);die;
 
         $url = "https://backend.api-wa.co/campaign/smartping/api/v2";
 
@@ -649,7 +649,7 @@ class EnquiryRequestController extends BaseController
                     $getEnquiry         = $this->data['model']->find_data('ecomm_enquires', 'row', ['ecomm_enquires.id' => $enquiry_id], 'ecomm_enquires.plant_id, ecomm_enquires.gps_tracking_image, ecomm_enquires.enquiry_no, ecomm_users.plant_name, ecomm_users.full_address', $join1);
                     $image              =   [
                                                 "url" => (($getEnquiry)?base_url('public/uploads/enquiry/' . $getEnquiry->gps_tracking_image):'https://commodity.ecoex.market/public/uploads/1700637387admin_leftlogo.png'),
-                                                "filename" => (($getEnquiry)?$getEnquiry->gps_tracking_image:'ImageName')
+                                                "filename" => (($getEnquiry)?$getEnquiry->gps_tracking_image:'ImageName.jpg')
                                             ];
 
                     $join2[0]            = ['table_master' => 'ecomm_enquiry_products', 'field_table_master' => 'product_id', 'table' => 'ecomm_company_items', 'field' => 'id', 'type' => 'LEFT'];
