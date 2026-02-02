1b4f3ac2e6d4a54b6f35841fc9d36bd6b4c61d27
subhomoy samanta <subhomoy@keylines.net>
2026-02-02 14:58:44 +0530

    send wp messahe selected enquiry state

--- app/Controllers/Admin/EnquiryRequestController.php
+++ app/Controllers/Admin/EnquiryRequestController.php
@@ -233,7 +233,9 @@ class EnquiryRequestController extends BaseController
                 foreach($getEnquiryItems as $getEnquiryItem){
                     $itemNameArray[] = $getEnquiryItem->item_name;
                     $quantityArray[] = $getEnquiryItem->qty . ' ' . $getEnquiryItem->unit_name;
-                    $priceRangeArray[] = (($getEnquiryItem->price_range != '')?'₹' . $getEnquiryItem->price_range:'');
+                    if($getEnquiryItem->price_range != ''){
+                        $priceRangeArray[] = (($getEnquiryItem->price_range != '')?'₹' . $getEnquiryItem->price_range:'');
+                    }
                 }
             }
 
