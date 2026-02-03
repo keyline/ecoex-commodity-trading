b55276d488be83829aea30a83efb964ae73fa0af
subhomoy samanta <subhomoy@keylines.net>
2026-02-03 20:37:44 +0530

    sidebar fixing for company account

--- app/Views/admin/elements/sidebar.php
+++ app/Views/admin/elements/sidebar.php
@@ -84,9 +84,12 @@ if ($userType == 'MA') {
     // 1️⃣ Define reusable function
     function filterByPlantId($dataArray, $plantIds)
     {
-        $filtered = array_filter($dataArray, function($item) use ($plantIds) {
-            return in_array($item->plant_id, $plantIds); // use array key since find_data returns array
-        });
+        $filtered = [];
+        if(!empty($plantIds)){
+            $filtered = array_filter($dataArray, function($item) use ($plantIds) {
+                return in_array($item->plant_id, $plantIds); // use array key since find_data returns array
+            });
+        }
 
         return array_values($filtered);
     }
