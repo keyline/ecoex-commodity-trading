c94a3ac9df5a7ac943e7866ba177c0857ccc7265
subhomoy samanta <subhomoy@keylines.net>
2026-02-25 12:36:01 +0530

    price list api updated

--- app/Controllers/Api/ApiController.php
+++ app/Controllers/Api/ApiController.php
@@ -375,7 +375,7 @@ class ApiController extends BaseController
             // Final array with average price
             $final = [];
             foreach ($result as $item) {
-                $average_price = round($item['total_price'] / $item['count'], 2);
+                $average_price = (($item['total_price'] != '')?round($item['total_price'] / $item['count'], 2):0);
                 if($average_price > 0){
                     $final[] = [
                         'icon'                                  => $item['icon'],
